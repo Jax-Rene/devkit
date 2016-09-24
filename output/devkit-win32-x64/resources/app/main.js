4942f6278937d3076e6db60b101b11251b0ce24a
庄静扬 <zhuangjy@chinanetcenter.com>
2016-09-24 20:52:26 +0800

    完善README.md

---
+++ output/devkit-win32-x64/resources/app/main.js
@@ -0,0 +1,33 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 800, height: 620 })
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+        // mainWindow.webContents.openDevTools()
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+}
+
+app.on('ready', function() {
+    createWindow()
+})
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
+
