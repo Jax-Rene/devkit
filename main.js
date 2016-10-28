541b7cb70b64af8838ec6ffda63145863f80afb8
zhuangjy <zhuangjy@chinanetcenter.com>
2016-10-28 23:42:05 +0800

    添加README软件截图

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let mainWindow
 function createWindow() {
     mainWindow = new BrowserWindow({ width: 800, height: 620 })
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
-    mainWindow.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools()
     mainWindow.on('closed', function() {
     mainWindow = null
     })
